be886ae66e0223fc1e71fefc914263892bb0c164
robert-kratz <60996025+robert-kratz@users.noreply.github.com>
2021-02-21 13:18:32 +0100

    implemented client cookie login

--- public/assets/bundle.js
+++ public/assets/bundle.js
@@ -1,5 +1,32 @@
 "use strict";
 
-document.onload = () => {
-    
+window.onload = () => {
+    try {
+        //implementing listener
+        document.getElementById('age').addEventListener('input', () => {
+            document.getElementById('age-display').innerHTML = document.getElementById('age').value;
+        });
+
+        document.getElementById('submit-register').addEventListener('click', () => {
+            //parsing input from form
+
+            var username = document.getElementById('name').value, age = document.getElementById('age').value, error = document.getElementById('error-message');
+            if(!(username.length > 2 && username.length < 64)) {
+                error.innerHTML = 'Your name has to be at least 2 characters';
+                return;
+            };
+            if(!(age > 2 && age < 100)) {
+                error.innerHTML = 'Your age has to be between 3 and 99';
+            };
+
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.open( "POST", 'api/register', false);
+            xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            xmlHttp.send(JSON.stringify({name: username, age: age, date: new Date().now}));
+
+            window.location.replace("solve"); //redirect user
+        });   
+    } catch (error) {
+        
+    }
 }
--- router/api.js
+++ router/api.js
@@ -9,7 +9,7 @@ const checkSession =  (req, res, next) => {
     next();
 };
 
-router.get('/register', checkSession, (req, res) => {
+router.post('/register', (req, res) => {
     if(req.body.name === undefined && req.body.age === undefined) res.status(400).json({"status": 400, "message": "Invalid request, it should contain name and age"});
 
     var data = {
@@ -17,10 +17,11 @@ router.get('/register', checkSession, (req, res) => {
         age: req.body.age,
         date: Date.now()
     };
+    console.log('New user registered: ' + JSON.stringify(data));
 
     res.cookie("user", data, {expire: (60000 * 60 * 24 * 1) + Date.now()}); //Cookie expires after 24h
 
-    res.status(200).json({"status": 200, "message": "Resouce created, ready for redirect"});
+    res.status(200).json({"status": 200, "message": "Cookie set, ready for redirect"});
 });
 
 router.all('/*', checkSession, (req, res) => {
